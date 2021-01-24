4ef07526d6739b4511b09dcd947b2c3a2cece187
Yegor Chsherbakov <76652963+yegorch@users.noreply.github.com>
2021-01-24 13:08:12 +0600

    Add files via upload

---
+++ components/TaskBlock.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="card">
+    <h2 class="card-title">
+      {{ task.title }}
+      <AppStatus :status="task.status" />
+    </h2>
+    <p>
+      <strong>
+        <small>
+          {{task.date}}
+        </small>
+      </strong>
+    </p>
+    <button class="btn primary" @click.prevent="$router.push(`/task/${id}`)">Посмотреть</button>
+  </div>
+</template>
+
+<script>
+import AppStatus from '../components/AppStatus'
+
+export default {
+  props: ['id', 'task'],
+  components: {AppStatus}
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ components/TheNavbar.vue
@@ -0,0 +1,57 @@
+<template>
+  <header class="navbar">
+    <h3>Freelance</h3>
+
+    <ul class="navbar-menu">
+      <li>
+        <router-link to="/">Все задачи</router-link>
+      </li>
+      <li>
+        <router-link to="/new">Создать</router-link>
+      </li>
+    </ul>
+  </header>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+
+.navbar {
+  display: flex;
+  padding: 0 2rem;
+  justify-content: space-between;
+  align-items: center;
+  position: fixed;
+  z-index: 20;
+  top: 0;
+  left: 0;
+  right: 0;
+  height: 56px;
+  background-color: #fff;
+  box-sizing: border-box;
+  border-bottom: 1px solid #eaecef;
+}
+
+.navbar-menu {
+  list-style: none;
+  display: flex;
+}
+
+.navbar-menu li {
+  margin-right: 2rem;
+}
+
+.navbar-menu li a {
+  color: #2c3e50;
+  text-decoration: none;
+}
+
+.navbar-menu li a:hover, .navbar-menu li a.active {
+  border-bottom: 2px solid #3eaf7c;
+}
+
+</style>
