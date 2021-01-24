ceef661a61f64e0d04455973e6b73622c2ad7ca2
Yegor Chsherbakov <76652963+yegorch@users.noreply.github.com>
2021-01-24 13:06:20 +0600

    Update router.js

--- router.js
+++ router.js
@@ -2,15 +2,14 @@ import {createRouter, createWebHistory} from 'vue-router'
 import Tasks from '@/views/Tasks'
 import New from '@/views/New'
 import Task from '@/views/Task'
-import NoPage from '@/views/NoPage'
+
 
 export default createRouter({
   history: createWebHistory(),
   routes: [
     {path: '/', component: Tasks},
     {path: '/new', component: New},
-    {path: '/task/:taskId', component: Task},
-    {path: '/:notFound(.*)', component: NoPage}
+    {path: '/task/:taskId', component: Task}
   ],
   linkActiveClass: 'active',
   linkExactActiveClass: 'active'
