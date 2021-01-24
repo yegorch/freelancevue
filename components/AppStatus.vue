5e2c9f32f5a1b3c558ddd992161041ad3475d143
Yegor Chsherbakov <76652963+yegorch@users.noreply.github.com>
2021-01-24 13:07:49 +0600

    Create AppStatus.vue

---
+++ components/AppStatus.vue
@@ -0,0 +1,11 @@
+<template>
+  <span :class="{ badge: true, primary: status === 'Активен' || status === 'Завершен', danger: status === 'Отменен', warning: status === 'Выполняется'}"
+  >{{ status }}</span>
+</template>
+
+<script>
+export default {
+  props: ['status']
+}
+</script>
+
